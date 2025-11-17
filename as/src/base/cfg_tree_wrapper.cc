93a5dbeafec59cf376feb807c90928f28189cecd
dwelch-spike <53876192+dwelch-spike@users.noreply.github.com>
2025-11-17 09:10:18 -0800

    AER-6861 - Add missing license and remove unused include.

--- as/src/base/cfg_tree_wrapper.cc
+++ as/src/base/cfg_tree_wrapper.cc
@@ -1,6 +1,27 @@
-#include <iostream>
-#include <string>
+/*
+ * cfg_tree_wrapper.cc
+ *
+ * Copyright (C) 2025 Aerospike, Inc.
+ *
+ * Portions may be licensed to Aerospike, Inc. under one or more contributor
+ * license agreements.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
 #include <cstring>
+#include <string>
 
 #include "base/cfg_tree.hpp"
 #include "base/cfg_tree_handlers.hpp"
