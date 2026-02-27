10b5b82365e9a72b4f7bfb38eb7ad978dd64d89f
Siggi <github@icellan.net>
2026-02-27 11:02:03 +0100

    Updated copyright and license

--- modules/mod-teranode/src/main/mod_teranode_utxo.c
+++ modules/mod-teranode/src/main/mod_teranode_utxo.c
@@ -1,7 +1,20 @@
 /*
  * mod_teranode_utxo.c
  *
- * Copyright (C) 2024 Aerospike, Inc.
+ * Copyright (C) 2026 BSV Association.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
  *
  * UTXO function implementations.
  * Direct port from teranode.lua to C.
